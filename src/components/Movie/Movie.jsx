967eb5289fe3312dbefd33b1c4a1fa65b57229bf
Liliana Leiva <liliana-carmen@hotmail.com>
2022-06-02 15:52:46 -0300

    fav details card

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,79 +1,83 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { getMovieDetail, addMovieFavorite } from '../../redux/actions';
-import { BsBookmarkStar, BsArrowLeftSquare } from 'react-icons/bs'
+import { BsBookmarkStar, BsArrowLeftSquare } from 'react-icons/bs';
+import Loading from '../Loading/Loading.jsx';
 
-class Movie extends React.Component {
 
-  componentDidMount() {
-    this.props.getMovieDetail(this.props.match.params.id)
-  }
+function Movie() {
+  const { id } = useParams()
+  const dispatch = useDispatch()
 
-  render() {
-    return (
-      <div className='container mx-auto flex flex-col'>
-        <Link to="/">
-          <button className='mb-4 text-red-300 hover:text-red-400 text-3xl mt-40'>
-            <BsArrowLeftSquare/>
-          </button>
-        </Link>
-        <div className='flex flex-row bg-gray-600 rounded-xl mt-4'>
-          <div>
-            <img className='w-96 h-full object-cover rounded-l-xl' src={this.props.movieDetail.Poster} alt="movie poster" />
-          </div>
-          <div className='ml-10'>
-            <div className='flex flex-row'>
-              <h3 className='text-2xl font-bold text-red-400'>{this.props.movieDetail.Title}</h3>
-              <button className="text-red-300 hover:text-red-400 text-2xl ml-4" onClick={() => {
-                this.props.addMovieFavorite({
-                  imdbID: this.props.movieDetail.imdbID,
-                  Title: this.props.movieDetail.Title,
-                  Year: this.props.movieDetail.Year,
-                  Type: this.props.movieDetail.Type,
-                  Poster: this.props.movieDetail.Poster
-                })
-                alert(`${this.props.movieDetail.Title} was added to favorites`)
-              }
-              }>                
-                <BsBookmarkStar/>
-              </button>
+  const [favorite, setFavorite] = useState(false);
+
+  useEffect(() => {
+    dispatch(getMovieDetail(id))
+  }, [dispatch])
+
+  const movieDetail = useSelector((state) => state.movieDetail)
+
+  return (
+    <div className='container mx-auto flex flex-col'>
+      {
+        favorite
+          ? <div className='flex flex-row justify-center'>
+            <div className="z-20 mt-52 fixed">
+              <div className="bg-white border-l-4 border-teal-500 text-teal-900 p-4 w-64 rounded-xl" role="alert">
+                <p className="font-bold text-center"> ✔ Movie added to favorites</p>
+              </div>
             </div>
-            <br/>
-            <p className='text-xl'>{this.props.movieDetail.Plot}</p>
-            <br/>
-            <p>Released: {this.props.movieDetail.Released}</p>
-            <br/>
-            <p>Genre: {this.props.movieDetail.Genre}</p>
-            <br/>
-            <p>Director: {this.props.movieDetail.Director}</p>
-            <br/>
-            <p>Running time: {this.props.movieDetail.Runtime}</p>
-            <br/>
-            <p>Writer: {this.props.movieDetail.Writer}</p>
-            <br/>
-            <p>Actors: {this.props.movieDetail.Actors}</p>
           </div>
-        </div>
-      </div>
-    );
-  }
+          : null
+      }
+      {
+        (!movieDetail.Poster && !movieDetail.Title && !movieDetail.Plot && !movieDetail.Released)
+          ? <div className='flex flex-row justify-center mt-24'><Loading /></div>
+          : <div>
+            <Link to="/">
+              <button className='mb-4 text-red-300 hover:text-red-400 text-3xl mt-40'>
+                <BsArrowLeftSquare />
+              </button>
+            </Link>
+            <div className='flex flex-row bg-gray-600 rounded-xl mt-4'>
+              <div>
+                <img className='w-96 h-full object-cover rounded-l-xl' src={movieDetail.Poster} alt="movie poster" />
+              </div>
+              <div className='ml-10'>
+                <div className='flex flex-row'>
+                  <h3 className='text-2xl font-bold text-red-400'>{movieDetail.Title}</h3>
+                  <button className="text-red-300 hover:text-red-400 text-2xl ml-4" onClick={() => {
+                    dispatch(addMovieFavorite(movieDetail))
+                    setFavorite(true)
+                    setTimeout(() => {
+                      setFavorite(false)
+                    }, 1500)
+                  }}>
+                    <BsBookmarkStar />
+                  </button>
+                </div>
+                <br />
+                <p className='text-xl'>{movieDetail.Plot}</p>
+                <br />
+                <p>Released: {movieDetail.Released}</p>
+                <br />
+                <p>Genre: {movieDetail.Genre}</p>
+                <br />
+                <p>Director: {movieDetail.Director}</p>
+                <br />
+                <p>Running time: {movieDetail.Runtime}</p>
+                <br />
+                <p>Writer: {movieDetail.Writer}</p>
+                <br />
+                <p>Actors: {movieDetail.Actors}</p>
+              </div>
+            </div >
+          </div>
+      }
+    </div >
+  );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    movieDetail: state.movieDetail
-  }
-}
+export default Movie;
 
-function mapDispatchToProps(dispatch) {
-  return {
-    addMovieFavorite: movie => dispatch(addMovieFavorite(movie)),
-    getMovieDetail: movie => dispatch(getMovieDetail(movie))
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Movie);
